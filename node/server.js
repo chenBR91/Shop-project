4872e417ce8454a91f6104eddd732e0495063928
Chen <chenbrown91@gmail.com>
2023-01-17 22:19:40 +0200

    change in call to server fetch

---
+++ node/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import productsServer from "./data/productsServer.js";
+
+const PORT = 8000;
+
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+app.get('/about', async (req, res) => {
+    res.send({message: 'Hello world'});
+})
+
+app.get('/', async(req, res) => {
+    res.send({data: productsServer});
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server lisiting on port ${PORT}`);
+})
